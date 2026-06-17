adcc01b13c4c5be807cbd274e54949e8e5bb2f05
tulyo-zynga123 <thiago.duclos@escola.pr.gov.br>
2026-06-17 10:43:43 -0300

    Update exercicio02.js

--- listas/lista 1/exercicio02.js
+++ listas/lista 1/exercicio02.js
@@ -1,1 +1,8 @@
+const base = 10;
+const altura = 5;
 
+const area = base * altura;
+const perimetro = 2 * (base + altura);
+
+console.log(`Área do retângulo: ${area}`);
+console.log(`Perímetro do retângulo: ${perimetro}`);
