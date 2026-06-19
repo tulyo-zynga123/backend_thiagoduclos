28183c3bb50b1de93953c27dd5fab764881997d7
tulyo-zynga123 <thiago.duclos@escola.pr.gov.br>
2026-06-19 17:18:35 -0300

    Update exercicio12.js

--- listas/lista 1/exercicio12.js
+++ listas/lista 1/exercicio12.js
@@ -1,1 +1,15 @@
+function calcular(a, b, operacao) {
+    return operacao(a, b);
+}
 
+console.log(calcular(10, 5, (a, b) => {
+    return a + b;
+}));
+
+console.log(calcular(10, 5, (a, b) => {
+    return a - b;
+}));
+
+console.log(calcular(10, 5, (a, b) => {
+    return a * b;
+}));
