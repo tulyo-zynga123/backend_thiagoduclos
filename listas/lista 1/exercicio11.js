3e399fe5c2b95e19ca1fb4324f2759a803d2e5a2
tulyo-zynga123 <thiago.duclos@escola.pr.gov.br>
2026-06-19 12:19:03 -0300

    Update exercicio11.js

--- listas/lista 1/exercicio11.js
+++ listas/lista 1/exercicio11.js
@@ -1,4 +1,10 @@
-let PodeVotar (idade) 
+const ler = require('readline-sync');
 
-const
-
+function PodeVotar (idade) {
+    return  idade => 16
+}
+const idade = ler.question ();
+if (PodeVotar(idade) >16){
+    console.log ("true")
+}
+else {console.log ("false")}
