b8ea5a10bc4918e1b9d5bc7058896fa47624c324
tulyo-zynga123 <thiago.duclos@escola.pr.gov.br>
2026-06-24 08:53:21 -0300

    Update ex01.js

---
+++ aulas/aula 1/ex01.js
@@ -0,0 +1,6 @@
+const aluno = { nome: "Ana", idade: 17, cidade: "Cascavel" };
+console.log(aluno.nome); 
+console.log(aluno.idade); 
+console.log(aluno.cidade); 
+
+console.log(`${aluno.nome} tem ${aluno.idade} anos.`);
