0527c1012cbbc59559079cd30971d3147d4bde02
tulyo-zynga123 <thiago.duclos@escola.pr.gov.br>
2026-07-07 08:34:03 -0300

    Add arithmetic operations to ex02.js

--- aulas/aula 1/ex02.js
+++ aulas/aula 1/ex02.js
@@ -1,3 +1,24 @@
 let num1 = Number(prompt("digite o primeiro numero:")); 
 let num2 = Number(prompt("digite o segundo numero:")); 
 console.log(`soma: ${num1 + num2}`);
+
+//subtração//
+
+let num1 = Number(prompt("digite o primeiro numero:")); 
+let num2 = Number(prompt("digite o segundo numero:")); 
+console.log(`subtração: ${num1 - num2}`);
+
+//multiplicação//
+
+let num1 = Number(prompt("digite o primeiro numero:")); 
+let num2 = Number(prompt("digite o segundo numero:")); 
+console.log(`multiplicação: ${num1 * num2}`);
+
+//divisão//
+
+let num1 = Number(prompt("digite o primeiro numero:")); 
+let num2 = Number(prompt("digite o segundo numero:")); 
+if (num2 === 0) {
+console.log("erro: divisão por zero não é possivel.");
+} else {
+console.log(`divisão: ${num1 / num2}`);
