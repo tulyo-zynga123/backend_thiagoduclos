76abd2a5ac89f0f8dc65da1937dd04f76627b147
tulyo-zynga123 <thiago.duclos@escola.pr.gov.br>
2026-07-07 08:51:50 -0300

    Update ex05.js

--- aulas/aula 1/ex05.js
+++ aulas/aula 1/ex05.js
@@ -1,3 +1,9 @@
-let numero = 7;
-let resultado = (numero % 2 === 0) ? "par" : "impar";
-console.log(resultado);
+for (let i = 1; i <= 10; i++) {
+  let numero = Number(prompt('digite o ${i}° numero:'));
+
+if (numero % 2 === 0) {
+  console.log('${numero} é par.'));
+} else {
+  console.log('${numero} é impar.');
+}
+}
