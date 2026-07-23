c592c108e09e31324f3154a4fde759bb02a587d3
tulyo-zynga123 <thiago.duclos@escola.pr.gov.br>
2026-07-23 08:35:33 -0300

    Add guessing game functionality in ex06.js
    Implement guessing game logic with user input.

--- aulas/aula 1/ex06.js
+++ aulas/aula 1/ex06.js
@@ -1,5 +1,21 @@
-const secreto = 42
-while (secreto <= 41){
-  console.log ('O numero secreto é maior')
+const readline = require('readline-sync');
+
+const secreto = 42;
+let tentativas = 0;
+let acertou = false;
+
+console.log("=== jogo da adivinhação ===");
+
+while (!acertou) {
+    let palpite = Number(readline.question("digite o seu palpite: "));
+    tentativas++;
+
+    if (palpite === secreto) {
+        console.log(`parabéns! Você acertou em ${tentativas} tentativa(s).`);
+        acertou = true;
+    } else if (secreto > palpite) {
+        console.log("o número secreto é maior!");
+    } else {
+        console.log("o número secreto é menor!");
+    }
 }
- while (secre
