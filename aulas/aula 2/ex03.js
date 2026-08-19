88960c343ada6b67730b601e04a13a3ccbff3f87
Thiago <thiago.duclos@escola.pr.gov.br>
2026-08-19 17:45:41 -0300

    Implement basic arithmetic functions in ex03.js
    Added functions for basic arithmetic operations: addition, subtraction, multiplication, and division.

---
+++ aulas/aula 2/ex03.js
@@ -0,0 +1,19 @@
+const somar = (a , b ) = > {
+return a + b ;
+console.log(`soma: ${num1 + num2}`);
+};
+
+const subtrair = (a , b ) = > {
+return a - b ;
+console.log(`soma: ${num1 - num2}`);
+};
+
+const multiplicar = (a , b ) = > {
+return a  * b ;
+console.log(`soma: ${num1 * num2}`);
+};
+
+const dividir = (a , b ) = > {
+return a / b ;
+console.log(`soma: ${num1 / num2}`);
+};
